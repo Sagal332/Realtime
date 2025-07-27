c99e209d7023abc97d540e4a2cf00a8cc453b3fc
hp <hp@DESKTOP-VESBO0D>
2025-07-27 22:24:00 +0300

    Updated files

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
@@ -10,8 +11,8 @@ def index():
 
 @socketio.on('message')
 def handle_message(msg):
-    print(f'Message: {msg}')
+    print(f"Message received: {msg}")
     send(msg, broadcast=True)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     socketio.run(app, debug=True)
